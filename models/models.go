cff253e2c81ed3972eb9d04126db06c3e80eeb8b
BrayanPerez2607 <brayperez2607@gmail.com>
2024-10-07 16:38:59 -0500

    2nd section - API connected to MongoDB

--- models/models.go
+++ models/models.go
@@ -1,7 +1,9 @@
 package models
 
+import "go.mongodb.org/mongo-driver/bson/primitive"
+
 type ToDo struct {
-	ID        int    `json:"id"`
-	Completed bool   `json:"completed"`
-	Body      string `json:"body"`
+	ID        primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
+	Completed bool               `json:"completed"`
+	Body      string             `json:"body"`
 }
